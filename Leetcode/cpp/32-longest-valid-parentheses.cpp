55a181f38c6f6811550a3d2075aa5473bd06dbb9
Sundaram Dubey <sundaram.dubey@swiggy.in>
2025-10-22 06:54:17 +0530

    feat: Add complete solution for 32. Longest Valid Parentheses in Java, Python, and C++

--- Leetcode/cpp/32-longest-valid-parentheses.cpp
+++ Leetcode/cpp/32-longest-valid-parentheses.cpp
@@ -1,1 +1,29 @@
 // Solution for 32. Longest Valid Parentheses
+#include <string>
+#include <stack>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        stack<int> stk;
+        stk.push(-1);
+        int maxLength = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '(') {
+                stk.push(i);
+            } else {
+                stk.pop();
+                if (stk.empty()) {
+                    stk.push(i);
+                } else {
+                    maxLength = max(maxLength, i - stk.top());
+                }
+            }
+        }
+        
+        return maxLength;
+    }
+};
